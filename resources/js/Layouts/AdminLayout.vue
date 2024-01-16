0d54cb813de4eaa8c62358b3d325d4880e55ad95
Konyuka <michaelsaiba84@gmail.com>
2024-01-16 17:27:29 +0300

    log

--- resources/js/Layouts/AdminLayout.vue
+++ resources/js/Layouts/AdminLayout.vue
@@ -33,6 +33,7 @@ const logout = () => {
             enter-from-class="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
             enter-to-class="translate-y-0 opacity-100 sm:translate-x-0" leave-from-class="opacity-100"
             leave-to-class="opacity-0" leave-active-class="transition ease-in duration-100">
+            
             <div v-if="launchToaster" aria-live="assertive"
                 class="z-[1000] pointer-events-none fixed inset-0 flex items-end px-4 py-6 sm:items-start sm:p-6">
                 <div class="flex w-full flex-col items-center space-y-4 sm:items-end">
